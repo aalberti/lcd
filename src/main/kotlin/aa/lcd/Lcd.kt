ae6174c54c3bea2070c405f2e31343ac7b86f3da
aalberti <aa@antoinealberti.com>
2018-08-31 10:14:48 +0200

    create class Lcd

--- src/main/kotlin/aa/lcd/Lcd.kt
+++ src/main/kotlin/aa/lcd/Lcd.kt
@@ -1,81 +1,85 @@
 package aa.lcd
 
-fun lcd(int: Int): String = lcd(int, """
-    |
-    |
-    |
-""".trimMargin())
+class Lcd {
+    fun display(int: Int): String {
+        return display(int, """
+            |
+            |
+            |
+        """.trimMargin())
+    }
 
-private tailrec fun lcd(int: Int, acc: String): String = when {
-    int == 0 -> """
+    private tailrec fun display(int: Int, acc: String): String = when {
+        int == 0 -> """
             | _ 
             || |
             ||_|
         """.trimMargin() concat acc
-    int / 10 == 0 -> char(int) concat acc
-    else -> lcd(int / 10, char(int % 10) concat acc)
-}
+        int / 10 == 0 -> char(int) concat acc
+        else -> display(int / 10, char(int % 10) concat acc)
+    }
 
-private infix fun String.concat(other: String): String =
-        (lines() zip other.lines())
-                .map { "${it.first}${it.second}" }
-                .joinToString("\n")
+    private infix fun String.concat(other: String): String =
+            (lines() zip other.lines())
+                    .map { "${it.first}${it.second}" }
+                    .joinToString("\n")
 
-private fun char(digit: Int): String = chars[digit] ?: """
+    private fun char(digit: Int): String = chars[digit] ?: """
         |   
         | \/
         | /\
     """.trimMargin()
 
-private val chars = mapOf(
-        0 to """
+    private val chars = mapOf(
+            0 to """
             | _ 
             || |
             ||_|
         """.trimMargin(),
-        1 to """
+            1 to """
             |   
             |  |
             |  |
         """.trimMargin(),
-        2 to """
+            2 to """
             | _ 
             | _|
             ||_ 
         """.trimMargin(),
-        3 to """
+            3 to """
             | _ 
             | _|
             | _|
         """.trimMargin(),
-        4 to """
+            4 to """
             |   
             ||_|
             |  |
         """.trimMargin(),
-        5 to """
+            5 to """
             | _ 
             ||_ 
             | _|
         """.trimMargin(),
-        6 to """
+            6 to """
             |   
             ||_ 
             ||_|
         """.trimMargin(),
-        7 to """
+            7 to """
             | _ 
             |  |
             |  |
         """.trimMargin(),
-        8 to """
+            8 to """
             | _ 
             ||_|
             ||_|
         """.trimMargin(),
-        9 to """
+            9 to """
             | _ 
             ||_|
             | _|
         """.trimMargin()
-)
+    )
+}
--- src/test/kotlin/aa/lcd/LcdTest.kt
+++ src/test/kotlin/aa/lcd/LcdTest.kt
@@ -5,35 +5,35 @@ import io.kotlintest.specs.StringSpec
 
 class LcdTest : StringSpec({
     "0" {
-        lcd(0) shouldBe """
+        Lcd().display(0) shouldBe """
             | _ 
             || |
             ||_|
         """.trimMargin()
     }
     "1" {
-        lcd(1) shouldBe """
+        Lcd().display(1) shouldBe """
             |   
             |  |
             |  |
         """.trimMargin()
     }
     "2" {
-        lcd(2) shouldBe """
+        Lcd().display(2) shouldBe """
             | _ 
             | _|
             ||_ 
         """.trimMargin()
     }
     "11" {
-        lcd(11) shouldBe """
+        Lcd().display(11) shouldBe """
             |      
             |  |  |
             |  |  |
         """.trimMargin()
     }
     "all in" {
-        lcd(1234567890) shouldBe """
+        Lcd().display(1234567890) shouldBe """
             |    _  _     _     _  _  _  _ 
             |  | _| _||_||_ |_   ||_||_|| |
             |  ||_  _|  | _||_|  ||_| _||_|
